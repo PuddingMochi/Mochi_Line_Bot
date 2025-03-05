b61b1533eeb0026775a45eeae6308e0f7bb4bb70
PuddingMochi <51309816+PuddingMochi@users.noreply.github.com>
2025-03-06 02:41:52 +0800

    Update url

--- app.py
+++ app.py
@@ -640,8 +640,9 @@ def handle_postback(event):
                 )
             )
         elif postback_data == 'send_image':
-            url = url_root + 'static/20210322_151237.JPG'
-            url = url.replace("http", "https")
+            url = 'https://drive.google.com/drive/u/2/folders/1uzeQYMA00DbK6CivR9_gdWFGvND3IsXm'
+            #url = url_root + 'static/20210322_151237.JPG'
+            #url = url.replace("http", "https")
             app.logger.info("url=" + url)
             line_bot_api.reply_message(
                 ReplyMessageRequest(
